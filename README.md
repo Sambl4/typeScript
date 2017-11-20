fbde23a0597e3e3526fb38d114df7997f22bea45
Dzmitry Samuilik <Sambl4@tut.by>
2017-11-21 01:28:10 +0300

    first task is started

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TypeScript-In-Depth
 
-1. > npm install -g typescript gulp
+1. > npm install typescript gulp
 2. > npm install
 3. > gulp
--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,14 @@ function showHello(divName: string, name: string) {
     elt.innerText = `Hello from ${name}`;
 }
 
+function getAllBooks() {
+	let books= [
+		{ title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
+		{ title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
+		{ title: 'CSS Secrets', author: 'Lea Verou', available: true },
+		{ title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true}
+	]
+	return books;
+}
+
+console.log(getAllBooks())
